e91f99e5cd488829aa17c87ed529726defa71b6d
Pavel Flaska <Pavel.Flaska@gmail.com>
2025-08-24 11:18:39 +0200

    ED25519 support - JDK-15 alternative
    Remove obsolete stuff. Simplyfing.

--- sshd-common/src/main/java/org/apache/sshd/common/signature/BuiltinSignatures.java
+++ sshd-common/src/main/java/org/apache/sshd/common/signature/BuiltinSignatures.java
@@ -237,7 +237,7 @@ public enum BuiltinSignatures implements SignatureFactory {
 
         @Override
         public boolean isSupported() {
-            return SecurityUtils.isEdEcJdkAvailable() || SecurityUtils.isEDDSACurveSupported();
+            return SecurityUtils.isEDDSACurveSupported();
         }
     },
     ed25519_cert(KeyPairProvider.SSH_ED25519_CERT) {
